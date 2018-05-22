dccd469514761c3c494f3511ccb77baa6571fd73
Jakob Leczinsky <jakob.leczinsky@gmail.com>
2018-05-22 15:59:00 +0200

    implemented greeting

--- client/home/home.js
+++ client/home/home.js
@@ -1,5 +1,12 @@
 function addGreetingTo(selection) {
-  throw 'Not Implemented!';
+  const url = new URL(document.location.href);
+  const name = url.searchParams.get('name');
+  const element = document.querySelector(selection);
+  element.innerHTML = `Welcome ${displayName(name)}!`;
+}
+
+function displayName(name) {
+  return name.charAt(0).toUpperCase() + name.slice(1);
 }
 
 module.exports.addGreetingTo = addGreetingTo;

5e176ba00e069ae7d7da82dea842c97a371256a3
Jakob Leczinsky <jakob.leczinsky@gmail.com>
2018-05-23 10:09:20 +0200

    use swapi in production mode

--- server/players/remote/remote-players-client.js
+++ server/players/remote/remote-players-client.js
@@ -1,4 +1,30 @@
-// Not Implemented!
-//
-// Hint: This module should have the same interface as the mock module.
-// Hint: Use the 'people' endpoint in SWAPI (https://swapi.co/api/).
+const https = require('https');
+
+const base = 'https://swapi.co/api/';
+
+function fetchAll(callback) {
+  get(base + 'people/', callback);
+}
+
+function getPlayer(id, callback) {
+  get(base + 'people/' + id + '/', callback);
+}
+
+function get(path, callback) {
+
+  https.get(path, res => {
+    
+    let data = '';
+
+    res.on('data', chunk => {
+      data += chunk;
+    });
+
+    res.on('end', () => {
+      callback(JSON.parse(data));
+    });
+  });
+}
+
+module.exports.fetchAll = fetchAll;
+module.exports.getPlayer = getPlayer;

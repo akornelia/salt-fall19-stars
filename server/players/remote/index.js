ccaac32f0595b67d47f072f7d413343054d027fc
Jakob Leczinsky <jakob.leczinsky@gmail.com>
2018-05-23 10:03:42 +0200

    swapi hints

--- server/players/remote/index.js
+++ server/players/remote/index.js
@@ -1,3 +1,4 @@
+// Hint: Require real implementation when running in production mode. Otherwise, use the mock!
 const playersHttpClient = require('./remote-players-client.mock');
 
 module.exports.playersHttpClient = playersHttpClient;
---
+++ server/players/remote/remote-players-client.js
@@ -0,0 +1,4 @@
+// Not Implemented!
+//
+// Hint: This module should have the same interface as the mock module.
+// Hint: Use the 'people' endpoint in SWAPI (https://swapi.co/api/).

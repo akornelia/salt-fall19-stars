acc475a16d5b3020536c849b4a670cbdc35ce2b8
Jakob Leczinsky <jakob.leczinsky@gmail.com>
2018-08-28 21:33:05 +0200

    added count to mock api

--- server/players/remote/remote-players-client.mock.js
+++ server/players/remote/remote-players-client.mock.js
@@ -1,13 +1,16 @@
 function fetchAll(callback) {
   console.log('MOCK', 'fetch all players');
   callback({
+    count: 2,
+    next: 'http://hocus.pocus/?page=2',
+    previous: null,
     results: [
       {
-        url: 'http://hocus.pucus/1/',
+        url: 'http://hocus.pocus/1/',
         name: 'joe',
       },
       {
-        url: 'http://hocus.pucus/2/',
+        url: 'http://hocus.pocus/2/',
         name: 'jane',
       },
     ],

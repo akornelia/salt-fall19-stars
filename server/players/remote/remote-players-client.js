baddef7bb5ddb9fa430b3d83a757c91564800a77
Jakob Leczinsky <jakob.leczinsky@gmail.com>
2018-09-06 21:09:12 +0200

    restructured exercise a little

--- server/players/remote/remote-players-client.js
+++ server/players/remote/remote-players-client.js
@@ -2,3 +2,31 @@
 //
 // Hint: This module should have the same interface as the mock module.
 // Hint: Use the 'people' endpoint in SWAPI (https://swapi.co/api/).
+const https = require('https');
+
+const base = '<url_to_swapi_api>';
+
+function fetchAll(callback) {
+  // Hint: Use the get(path, callback) function below with path to fetch all from SWAPI
+}
+
+function getPlayer(id, callback) {
+  // Hint: Use the get(path, callback) function below with path to fetch by id from SWAPI
+}
+
+function get(path, callback) {
+
+  https.get(path, res => {
+
+    res.on('data', chunk => {
+     	throw 'Not Implemented!';
+    });
+
+    res.on('end', () => {
+    	throw 'Not Implemented!';
+    });
+  });
+}
+
+module.exports.fetchAll = fetchAll;
+module.exports.getPlayer = getPlayer;

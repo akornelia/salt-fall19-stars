a7a71917368882be0646d6bd17e68c3f3044b8e1
christian aman <christian.aman@appliedtechnology.se>
2019-09-02 14:53:44 +0200

    Test added

--- client/home/home.spec.js
+++ client/home/home.spec.js
@@ -5,18 +5,27 @@ const home = require('./home');
 const { JSDOM } = jsdom;
 
 describe('The Home Page', () => {
-
-  beforeEach(() => {
-    const dom = new JSDOM('<!DOCTYPE html><html><body><section class="greeting"><h2>REPLACE ME!</h2></section></body></hmtl>', {
-      url: 'http://foo.bar/?name=joe',
-    });
+  function fetchDom(urlString = '') {
+    const dom = new JSDOM(
+      '<!DOCTYPE html><html><body><section class="greeting"><h2>Welcome!</h2></section></body></hmtl>',
+      {
+        url: `http://foo.bar/${urlString}`,
+      }
+    );
     global.document = dom.window.document;
     global.window = dom.window;
-  });
-
+  }
   it('should replace greeting for any named user', () => {
+    fetchDom('/?name=joe');
     home.addGreetingTo('.greeting > h2');
     const element = document.querySelector('.greeting > h2');
     assert.equal('Welcome Joe!', element.innerHTML);
+  });
+
+  it('should welcome an anonymous user', () => {
+    fetchDom();
+    home.addGreetingTo('.greeting > h2');
+    const element = document.querySelector('.greeting > h2');
+    assert.equal('Welcome!', element.innerHTML);
   });
 });

d9e1454a4bead345b1d401e626fc35dafc17ad67
Carl Månsson <carl.mnssn@gmail.copm>
2019-09-02 14:26:35 +0200

    Fixed by lessthanequals4

--- client/home/home.js
+++ client/home/home.js
@@ -1,5 +1,10 @@
 function addGreetingTo() {
-  throw new Error('Not Implemented!');
+  //  throw new Error('Not Implemented!');
+  const myName = new URLSearchParams(window.location.search).get('name');
+  const myGreeting = document.querySelector('.greeting > h2');
+  const myNameCap = myName.charAt(0).toUpperCase() + myName.slice(1);
+
+  myGreeting.textContent = `Welcome ${myNameCap}!`;
 }
 
 module.exports.addGreetingTo = addGreetingTo;

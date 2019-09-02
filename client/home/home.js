233a1661f4b3a47d3dc648ed6050610dc004607b
ariviegea <ariadna.villar.egea@gmail.com>
2019-09-02 17:13:26 +0200

    Fix errors

--- client/home/home.js
+++ client/home/home.js
@@ -1,5 +1,17 @@
-function addGreetingTo() {
-  throw new Error('Not Implemented!');
+function addGreetingTo(selector) {
+  //throw new Error('Not Implemented!');
+
+  const element = document.querySelector(selector);
+  const name = window.location.search.replace('?name=','');
+  let output;
+
+  if (name) {
+    output = 'Welcome ' + name.charAt(0).toUpperCase() + name.substring(1, name.length) + '!';
+  } else {
+    output = 'Welcome!';
+  }
+
+  element.innerHTML = output;
 }
 
 module.exports.addGreetingTo = addGreetingTo;

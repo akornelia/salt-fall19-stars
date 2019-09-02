094d557cc4338f1c9237e5ccf52eff12ca21eaee
Victor Stenström-Diaz <victor.stenstrom-diaz@appliedtechnology.se>
2019-09-02 13:27:48 +0200

    Got it working

--- client/home/home.js
+++ client/home/home.js
@@ -1,5 +1,13 @@
-function addGreetingTo() {
-  throw new Error('Not Implemented!');
+function capitalize(s) {
+  return s && s[0].toUpperCase() + s.slice(1);
+}
+
+function addGreetingTo(query) {
+  const urlParams = new URLSearchParams(document.location.search);
+  const name = urlParams.get('name');
+  if (name) {
+    document.querySelector(query).innerHTML = 'Welcome ' + capitalize(name) + '!';
+  }
 }
 
 module.exports.addGreetingTo = addGreetingTo;

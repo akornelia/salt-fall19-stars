3332d60a5d895196a5b1768d7241b4d2e9cbf2f1
pernybom <per.nybom@appliedtechnology.se>
2019-09-02 14:06:30 +0200

    Refactored

--- client/home/home.js
+++ client/home/home.js
@@ -4,9 +4,10 @@ function addGreetingTo(h2) {
   // set name variable from url param 'name'
   let name = new URLSearchParams(window.location.search).get('name');
   // capitalizing the first letter of the name and lowercases the rest of the name
-  name = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+  name = name ? ' ' + name.charAt(0).toUpperCase() + name.slice(1).toLowerCase() : '';
   // Selects the h2 and updates its innerHTML to include the name entered in the querystring
-  document.querySelector(h2).innerHTML = `Welcome ${name}!`;
+  document.querySelector(h2).innerHTML = `Welcome${name}!`;
+
 }
 
 //The function is exported.
